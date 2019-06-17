e03ca12b69c3c2927a30a76548cf7f7bb2406794
Van <549804563@qq.com>
2019-06-17 19:34:21 +0800

    update

--- src/main.js
+++ src/main.js
@@ -24,7 +24,13 @@ Vue.use(Popup);
 Vue.use(Toast);
 let options = {
   fullscreenEl:false,
-  tapToClose: true
+  tapToClose: true,
+  addCaptionHTMLFn: function(item, captionEl, isFake){
+    console.log(item)
+    if(item.src.indexOf('/thumb')>=0){
+      item.src = item.src.replace('/thumb',"")
+    }
+  } 
 }
 Vue.use(preview,options)
 Vue.use(VueLazyload,{
