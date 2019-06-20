3a32af2249dfc39a0a70651adaa274b335b85164
Van <549804563@qq.com>
2019-06-20 14:07:16 +0800

    update

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -1,5 +1,6 @@
 <script>
-let hostUrl = 'http://dev.kz.sync163.com/webpage/'
+let hostUrl = 'https://kz.sync163.com/webpage/'
+// let hostUrl = 'http://dev.kz.sync163.com/webpage/'
 // let hostUrl = 'https://dev.pdz.sync163.com/webpage/'
 let userInfo = ''
 let hasLogin = false
